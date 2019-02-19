b9d141cbeb28786c95ba271f54b469d2acb53e31
mathwer <mat-wer@hotmail.com>
2019-02-18 23:42:37 -0300

    Initial commit

---
+++ Assets/Animação.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animação : MonoBehaviour
+{
+    public FloatingJoystick joystick;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        if (moveVector != Vector3.zero) anim.SetTrigger("Andar");
+
+        else anim.SetTrigger("Parar");
+    }
+    public void BotaoAceno()
+    {
+        anim.SetTrigger("Acenar");
+    }
+
+    public void BotaoDanca()
+    {
+        anim.SetTrigger("Danca");
+    }
+
+    public void BotaoPular()
+    {
+        anim.SetTrigger("Jump");
+    }
+}
---
+++ Assets/JoyStick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    public FloatingJoystick joystick;
+    private Vector3 direction;
+    private float xMin, xMax, yMin, yMax;
+    Animator anim;
+    // Update is called once per frame
+
+    void Update()
+    {
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        while (moveVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveVector);
+            transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+            anim.SetTrigger("Andar");
+
+        }
+
+        if (moveVector == Vector3.zero)
+        {
+            anim.SetTrigger("Parar");
+        }
+
+
+
+    }
+}
